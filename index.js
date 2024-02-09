ac5c2769dbb82c01b6ac9c1898f7e764ebcec90e
alejosatizabal <alejosatizabal@yahoo.es>
2024-02-09 05:25:27 -0500

    Parte3 - Ejercicio 3.5

--- index.js
+++ index.js
@@ -60,6 +60,36 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    // const maxId = notes.length > 0
+    //   ? Math.max(...notes.map(n => n.id))
+    //   : 0
+    // return maxId + 1
+
+    // let numeroGenerado = Math.floor(Math.random()*1000)+1
+    return Math.floor(Math.random()*1000)+1
+  }
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+  
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
