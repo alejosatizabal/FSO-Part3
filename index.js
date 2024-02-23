81af4328952be665260963e84f59633586a59249
alejosatizabal <alejosatizabal@yahoo.es>
2024-02-22 19:29:05 -0500

    Parte3 - Ejercicio 3.17

--- index.js
+++ index.js
@@ -120,6 +120,21 @@ app.post('/api/persons', (request, response, next) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 // controlador de solicitudes con endpoint desconocido
 app.use(unknownEndpoint)
 
